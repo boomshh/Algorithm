9f2220fab5100eb40c54424a008d24b10ad8898c
Ethan <133577209+boomshh@users.noreply.github.com>
2023-11-18 23:49:28 +0900

    [D2] Title: 초심자의 회문 검사, Time: 96 ms, Memory: 42,724 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for tc in range(1, t + 1):
+    data = input()
+    temp = ''
+    for i in range(len(data) -1, -1, -1):
+        temp += data[i]
+    if data == temp:
+        print('#%d %d' % (tc, 1))
+    else:
+        print('#%d %d' % (tc, 0))
