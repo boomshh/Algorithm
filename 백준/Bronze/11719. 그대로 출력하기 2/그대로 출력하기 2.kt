8db560afec5513a57c24dbedf84865a60f8bbe8d
Ethan <133577209+boomshh@users.noreply.github.com>
2023-11-23 17:18:35 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 144 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+fun main() {
+    
+    val a = Scanner(System.`in`) 
+    while(a.hasNextLine()) {
+        val input = a.nextLine()
+        
+        println(input)
+    }
+    
+}
