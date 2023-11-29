763a4f8aa92e56489b45d98c1f2574da7e6641a6
Ethan <133577209+boomshh@users.noreply.github.com>
2023-11-29 18:58:07 +0900

    [Silver I] Title: 최대공약수, Time: 268 ms, Memory: 41664 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.kt
@@ -0,0 +1,10 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val(a, b) = readln().split(' ').map{ it.toLong()}
+    
+    val gcd = gcd(a, b)
+    println("1".repeat(gcd.toInt()))
+}
+
+fun gcd(a: Long, b: Long): Long {
+    return if(b == 0L) a else gcd(b, a % b)
+}
