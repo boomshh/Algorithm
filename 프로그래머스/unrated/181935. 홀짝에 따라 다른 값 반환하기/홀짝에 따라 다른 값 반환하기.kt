d407ef2be90b83e6cf85b37a0950f613b4dc799c
Ethan <133577209+boomshh@users.noreply.github.com>
2023-12-08 22:41:41 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 12.50 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(n: Int): Int {
+        return if(n % 2 == 1) {
+            (1..n step 2).sum()
+        } else {
+            (2..n step 2).sumOf { it * it }
+        }
+    }
+}
