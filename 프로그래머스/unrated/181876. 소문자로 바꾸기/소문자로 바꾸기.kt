dbadd2a8ba635fd9a7f16a34d26e3c1c4124cb89
Ethan <133577209+boomshh@users.noreply.github.com>
2023-12-08 20:16:06 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 2.28 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(myString: String): String {
+        var answer: String = ""
+        val a = myString.toLowerCase()
+        answer += a
+        return answer
+    }
+}
