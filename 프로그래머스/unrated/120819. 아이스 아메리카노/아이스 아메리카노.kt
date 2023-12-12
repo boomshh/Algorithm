a77db15118641b38e5de73cc4e6ea8af5fd5e4fa
Ethan <133577209+boomshh@users.noreply.github.com>
2023-12-12 21:24:23 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 9.72 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(money: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+
+        answer +=  money / 5500
+        answer += money % 5500
+        return answer
+    }
+}
