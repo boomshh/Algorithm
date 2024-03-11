4856656d11395384a8c6b52a5a50840fa61bd211
Ethan <133577209+boomshh@users.noreply.github.com>
2024-03-11 21:59:14 +0900

    [Bronze V] Title: 구구단, Time: 92 ms, Memory: 12264 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.kt
+++ 백준/Bronze/2739. 구구단/구구단.kt
@@ -1,7 +1,8 @@
 fun main() {
-    val n = readln().toInt()
-    for (i in 1..9) {
-        val m = n * i
-        println("$n * $i = $m")
+    val num = readln().toInt()
+
+    for(i in 1..9) {
+        val result = num * i
+        println("$num * $i = $result")
     }
 }
