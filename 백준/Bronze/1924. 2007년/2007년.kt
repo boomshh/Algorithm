0e9e4ce1654ef59f4654d175493cb2de1c1264db
Ethan <133577209+boomshh@users.noreply.github.com>
2024-03-15 21:46:56 +0900

    [Bronze I] Title: 2007년, Time: 136 ms, Memory: 21024 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.kt
+++ 백준/Bronze/1924. 2007년/2007년.kt
@@ -1,14 +1,15 @@
 fun main() {
-    val (x, y) = readLine()!!.split(" ").map { it.toInt() }
-    val daysInMonth = intArrayOf(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-    val daysOfWeek = arrayOf("SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT")
+    val (a, b) = readln().split(" ").map { it.toInt() }
+    val days = intArrayOf(0, 31, 28, 31, 30 ,31,30, 31,31, 30, 31, 30, 31)
+    val years = arrayOf("SUN","MON", "TUE", "WED","THU", "FRI", "SAT")
 
-    var totalDays = y
-    for (i in 1 until x) {
-        totalDays += daysInMonth[i]
+    var totalDays = b
+
+    for(i in 1 until a) {
+        totalDays += days[i]
     }
 
-    val dayOfWeek = (totalDays ) % 7
+    val dayOfWeek = totalDays % 7
 
-    println(daysOfWeek[dayOfWeek])
+    println(years[dayOfWeek])
 }
